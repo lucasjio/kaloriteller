ee8e5b1c7be12085abca479ac72e2c54aa100fe9
lucasjio <ljohnsen897@gmail.com>
2023-11-30 15:21:06 +0100

    first

---
+++ main.py
@@ -0,0 +1,51 @@
+import certifi
+from fastapi import FastAPI
+from typing import List
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import sys
+
+app = FastAPI()
+brukernavn = "lucasjio"
+passord = "mongodb_Vinterferie22;"
+uri = f"mongodb+srv://test:test@kaloriteller.xgvddyj.mongodb.net/?retryWrites=true&w=majority"
+
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'), tlsCAFile=certifi.where())
+mydb = client["mydatabase"]
+mycol = mydb["foods"]
+food_names = []
+
+@app.get("/food_names")
+async def read_items():
+    for x in mycol.find({},{"navn": 1, "_id": 0}):
+        food_names.append(x["navn"])
+    return food_names
+
+@app.get("/food_details/{food_name}")
+async def read_item(food_name: str):
+    food_details = mycol.find_one({"navn": food_name}, {"_id": 0})
+    # food_details = mycol.find_one({"navn": food_name})
+
+   
+    return food_details
+
+
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+
+
+
+
+
+
+
+
+
+
+
